cbdca0d96c0ed284aad38ef42b6862fee5d6145a
Gromet Spaal <gromms@users.noreply.github.com>
2025-03-12 23:05:26 +0200

    Add nvim-java

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,5 +1,7 @@
 local lsp_zero = require('lsp-zero')
 
+vim.filetype.add({ extension = { templ = "templ" } })
+
 lsp_zero.on_attach(function(client, bufnr)
     local opts = { buffer = bufnr, remap = false }
 
@@ -11,6 +13,7 @@ lsp_zero.on_attach(function(client, bufnr)
     end, opts)
     vim.keymap.set("n", "<leader>D", function() vim.lsp.buf.type_definition() end, opts)
     vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts, "Rename")
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
     vim.keymap.set("v", "<leader>ca", "<ESC><CMD>lua vim.lsp.buf.range_code_action()<CR>",
         { noremap = true, silent = true, buffer = bufnr, desc = "Code actions" })
     vim.keymap.set("n", "==", function() vim.lsp.buf.format { async = true } end, opts)
@@ -20,19 +23,19 @@ lsp_zero.on_attach(function(client, bufnr)
     vim.keymap.set("n", "<leader>vd", function() vim.lsp.buf.open_float() end, opts)
     vim.keymap.set("n", "[d", function() vim.lsp.buf.goto_next() end, opts)
     vim.keymap.set("n", "]d", function() vim.lsp.buf.goto_prev() end, opts)
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
     --vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>vrr", function() require('telescope.builtin').lsp_references() end, { noremap = true, silent = true })
+    vim.keymap.set("n", "<leader>vrr", function() require('telescope.builtin').lsp_references() end,
+        { noremap = true, silent = true })
     vim.keymap.set("n", "<C-s>", function() vim.lsp.buf.signature_help() end, opts)
     vim.keymap.set("n", "gn", function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set("n", "gp", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set('n', '<space>?', '<cmd>lua vim.diagnostic.open_float()<CR>', { buffer=bufnr, noremap=true, silent=true })
+    vim.keymap.set('n', '<space>?', '<cmd>lua vim.diagnostic.open_float()<CR>',
+        { buffer = bufnr, noremap = true, silent = true })
 end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
     ensure_installed = {
-        'tsserver',
         'eslint',
         'rust_analyzer',
         'kotlin_language_server',
@@ -43,16 +46,68 @@ require('mason-lspconfig').setup({
         'groovyls',
         'gradle_ls',
         'gopls',
-        'lua_ls'
+        'templ',
+        'lua_ls',
+        'jdtls'
     },
     handlers = {
         lsp_zero.default_setup,
-        lua_ls = function()
-            local lua_opts = lsp_zero.nvim_lua_ls()
-            require('lspconfig').lua_ls.setup(lua_opts)
-        end,
+    --    lua_ls = function()
+    --        local lua_opts = lsp_zero.nvim_lua_ls()
+    --        local lspc = require('lspconfig')
+    --        lspc.lua_ls.setup(lua_opts)
+    --    end,
     }
 })
+
+lspconfig = require('lspconfig')
+
+require("java").setup()
+lspconfig.jdtls.setup({
+    settings = {
+        java = {
+            configuration = {
+                runtimes = {
+                    {
+                        name = "OpenJDK-21",
+                        path = "/opt/homebrew/opt/openjdk@21"
+                    }
+                }
+            }
+        }
+    }
+})
+
+lspconfig.gopls.setup({
+    on_attach = lsp_zero.on_attach,
+    capabilities = lsp_zero.capabilities,
+    settings = {
+        gopls = {
+            analyses = {
+                unusedparams = true,
+                shadow = true
+            },
+            staticcheck = true,
+        },
+    },
+})
+
+lspconfig.ruby_lsp.setup({
+  init_options = {
+    formatter = 'standard',
+    linters = { 'standard' },
+  },
+})
+
+local configs = require('lspconfig.configs')
+configs.templ = {
+  default_config = {
+    cmd = { "templ", "lsp", "-http=localhost:7474", "-log=/Users/grom/templ.log" },
+    filetypes = { 'templ' },
+    root_dir = lspconfig.util.root_pattern("go.mod", ".git"),
+    settings = {},
+  },
+}
 
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
@@ -67,11 +122,13 @@ cmp.setup({
     mapping = cmp.mapping.preset.insert({
         ['C-p'] = cmp.mapping.select_prev_item(cmp_select),
         ['C-n'] = cmp.mapping.select_next_item(cmp_select),
-        ["<C-o>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-o>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true,
+        }),
         ["<C-Space>"] = cmp.mapping.complete(),
     }),
 
 })
-
 
 lsp_zero.setup()
--- nvim/lua/gromms/lazy.lua
+++ nvim/lua/gromms/lazy.lua
@@ -1,41 +1,60 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    { "folke/tokyonight.nvim", as = "tokyonight", config = function() 
-        vim.cmd("colorscheme tokyonight")
-    end
-    },
-    { 'rose-pine/neovim', name = 'rose-pine' },
+    --{ import = "lazyvim.plugins.extras.dap.core" },
+    "folke/tokyonight.nvim",
     {
-    'nvim-telescope/telescope.nvim', tag = '0.1.4',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
+        "rose-pine/neovim",
+        name = 'rose-pine',
+        config = function()
+            vim.cmd("colorscheme rose-pine")
+        end
     },
-    {"nvim-treesitter/nvim-treesitter"},
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.6',
+        -- or                              , branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    },
+    { "nvim-treesitter/nvim-treesitter" },
     "ThePrimeagen/harpoon",
     "mbbill/undotree",
-    {'williamboman/mason.nvim'},
-    {'williamboman/mason-lspconfig.nvim'},
-
-    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-    {'neovim/nvim-lspconfig'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/nvim-cmp'},
-    {'L3MON4D3/LuaSnip'},
-    "github/copilot.vim",
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
+    --{ "rcarriga/nvim-dap-ui",             dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } },
+    { 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
+    { 'neovim/nvim-lspconfig' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/nvim-cmp' },
+    { 'L3MON4D3/LuaSnip' },
+    {
+        "nvim-java/nvim-java",
+        config = false,
+        dependencies = {
+            {
+                "neovim/nvim-lspconfig",
+            }
+        }
+    },
     "tpope/vim-fugitive",
-    "mfussenegger/nvim-jdtls",
     "nvim-telescope/telescope-ui-select.nvim",
-    { "nvim-lualine/lualine.nvim", dependencies = {"nvim-tree/nvim-web-devicons"}}
+    { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } }
 })
