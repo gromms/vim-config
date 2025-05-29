950c9ba98e0f1cbed0e48533d4ca5752d6d02b12
Gromet Spaal <gromet.spaal@gmail.com>
2025-05-30 00:46:47 +0300

    Improve init script

--- init.sh
+++ init.sh
@@ -1,26 +1,59 @@
 #!/bin/bash
 
+common_deps=(tmux neovim ttf-jetbrains-mono-nerd fzf)
+linux_deps=(zsh zsh-completions ${common_deps[*]})
+macos_deps=(${common_deps[*]})
+
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    pacman -S --noconfirm zsh zsh-completions tmux neovim ttf-jetbrains-mono-nerd
+    echo "Running Linux init\n"
+    echo "-- Installing dependencies\n"
+    if command -v pacman 2>&1 >/dev/null
+    then
+        pacman -S --noconfirm "${linux_deps[*]}"
+    elif command -v pacman 2>&1 >/dev/null
+    then
+        apt-get install -y "${linux_deps[*]}"
+    else
+        echo "Could not find supported package manager\n"
+        exit 1
+    fi
+    echo "-- Setting default shell to zsh\n"
     chsh -s /usr/bin/zsh
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    brew install tmux neovim font-jetbrains-mono-nerd-font
+    echo "Running MacOS init\n"
+    echo "-- Checking xcode-select\n"
+    if ! command -v xcode-select 2>&1 >/dev/null
+    then
+        echo "xcode-select not installed. running installer\n"
+        echo "Rerun init after it has installed\n"
+        exit 1
+    fi
+    echo "-- Installing dependencies\n"
+    brew install "${macos_deps[*]}"
 else
-    return 1
+    echo "Unsupported OS\n"
+    exit 1
 fi
 
+echo "-- Installing starship zsh theme\n"
 curl -sS https://starship.rs/install.sh | sh -s -- -f
 
 base_path=$( realpath "$0"  )
 base_path=$( dirname "$base_path" )
 
-ln -s "$base_path"/nvim ~/.config/ 
+echo "-- Linking config files\n"
 
-mkdir ~/.local/scripts
+echo "---- Neovim: $base_path/nvim -> $HOME/.config\n"
+ln -s "$base_path"/nvim "$HOME"/.config
+
+mkdir "$HOME"/.local/scripts
 chmod +x "$base_path"/tmux/tmux-sessionizer
-ln -s "$base_path"/tmux/tmux-sessionizer ~/.local/scripts
+echo "---- Tmux sessionizer: $base_path/tmux/tmux-sessionizer -> $HOME/.local/scripts\n"
+ln -s "$base_path"/tmux/tmux-sessionizer "$HOME"/.local/scripts
 
-ln -s "$base_path"/tmux/.tmux.config ~/
+echo "---- Tmux config: $base_path/tmux/.tmux.config -> $HOME\n"
+ln -s "$base_path"/tmux/.tmux.config "$HOME"
 
 mkdir ~/.cache/zsh
-ln -s "$base_path"/zsh/.zshrc ~/
+echo "---- ZSH: $base_path/zsh/.zshrc -> $HOME\n"
+ln -s "$base_path"/zsh/.zshrc "$HOME"
