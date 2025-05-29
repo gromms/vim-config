b44bb2f936a8e43bb932e43891eb6e780873d3ae
Gromet Spaal <gromet.spaal@gmail.com>
2025-05-29 20:45:10 +0300

    Add init script

---
+++ init.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    pacman -S --noconfirm zsh zsh-completions tmux neovim ttf-jetbrains-mono-nerd
+    chsh -s /usr/bin/zsh
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install tmux neovim font-jetbrains-mono-nerd-font
+else
+    return 1
+fi
+
+
+curl -sS https://starship.rs/install.sh | sh -s -- -f
+
+base_path=$( realpath "$0"  )
+base_path=$( dirname "$base_path" )
+
+ln -s "$base_path"/nvim ~/.config/ 
+
+mkdir ~/.local/scripts
+chmod +x "$base_path"/tmux/tmux-sessionizer
+ln -s "$base_path"/tmux/tmux-sessionizer ~/.local/scripts
+
+ln -s "$base_path"/tmux/.tmux.config ~/
+
+mkdir ~/.cache/zsh
+ln -s "$base_path"/zsh/.zshrc ~/
