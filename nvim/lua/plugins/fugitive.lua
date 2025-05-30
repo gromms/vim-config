89d80ad5757ab6c5987a6a5f7d1bd3774180d590
Gromet Spaal <gromms@users.noreply.github.com>
2025-05-30 13:54:40 +0300

    Add git blame keybind, change lualine section separators

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -3,6 +3,7 @@ return {
         "tpope/vim-fugitive",
         config = function() 
             vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+            vim.keymap.set("n", "<leader>gb", '<cmd>Git blame<CR>')
         end
     }
 }
