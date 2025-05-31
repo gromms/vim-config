e3dab05528aade4be36a0655973092170eec6e59
Gromet Spaal <gromet.spaal@gmail.com>
2025-05-31 23:31:27 +0300

    Add yank highlight

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -26,3 +26,18 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
