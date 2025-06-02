28e0f3eacccdce8fa8deb659dd7d1c03b66ae880
Gromet Spaal <gromet.spaal@gmail.com>
2025-06-02 09:43:14 +0300

    Fix bash array usage

--- init.sh
+++ init.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 common_deps=(tmux neovim ttf-jetbrains-mono-nerd fzf)
-linux_deps=(zsh zsh-completions ${common_deps[*]})
-macos_deps=(${common_deps[*]})
+linux_deps=(zsh zsh-completions ${common_deps[@]})
+macos_deps=(${common_deps[@]})
+
+home="${XDG_CACHE_HOME:-$HOME}"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     echo "Running Linux init\n"
     echo "-- Installing dependencies\n"
     if command -v pacman 2>&1 >/dev/null
     then
-        pacman -S --noconfirm "${linux_deps[*]}"
+        echo "Arch"
+        sudo pacman -S --noconfirm "${linux_deps[@]}"
     elif command -v pacman 2>&1 >/dev/null
     then
-        apt-get install -y "${linux_deps[*]}"
+        sudo apt-get install -y "${linux_deps[@]}"
     else
         echo "Could not find supported package manager\n"
         exit 1
@@ -39,7 +42,7 @@ else
 fi
 
 echo "Checking for tpm"
-if [ ! -f "$HOME/.tmux/tpm/tpm" ]; then
+if [ ! -f "$home/.tmux/tpm/tpm" ]; then
   echo "Tpm does not exists. Cloning repository"
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
@@ -52,17 +55,17 @@ base_path=$( dirname "$base_path" )
 
 echo "-- Linking config files\n"
 
-echo "---- Neovim: $base_path/nvim -> $HOME/.config\n"
-ln -s "$base_path"/nvim "$HOME"/.config
+echo "---- Neovim: $base_path/nvim -> $home/.config\n"
+ln -s "$base_path"/nvim "$home"/.config
 
-mkdir "$HOME"/.local/scripts
+mkdir "$home"/.local/scripts
 chmod +x "$base_path"/tmux/tmux-sessionizer
-echo "---- Tmux sessionizer: $base_path/tmux/tmux-sessionizer -> $HOME/.local/scripts\n"
-ln -s "$base_path"/tmux/tmux-sessionizer "$HOME"/.local/scripts
+echo "---- Tmux sessionizer: $base_path/tmux/tmux-sessionizer -> $home/.local/scripts\n"
+ln -s "$base_path"/tmux/tmux-sessionizer "$home"/.local/scripts
 
-echo "---- Tmux config: $base_path/tmux/.tmux.config -> $HOME\n"
-ln -s "$base_path"/tmux/.tmux.conf "$HOME"
+echo "---- Tmux config: $base_path/tmux/.tmux.conf -> $home\n"
+ln -s "$base_path"/tmux/.tmux.conf "$home"
 
 mkdir ~/.cache/zsh
-echo "---- ZSH: $base_path/zsh/.zshrc -> $HOME\n"
-ln -s "$base_path"/zsh/.zshrc "$HOME"
+echo "---- ZSH: $base_path/zsh/.zshrc -> $home\n"
+ln -s "$base_path"/zsh/.zshrc "$home"
